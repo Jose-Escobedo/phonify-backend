3abf955cebce0284fb6d53d7dd0afddf74e0104c
Jose <escobedo.jose48@outlook.com>
2022-05-25 17:59:57 -0700

    added sessions for signup and login auth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+    # Group Activity => Set 'authorize_user' to Skip Login Action
+
+    before_action :authorize_user, except: [:login]
+
+    def login
+        user = User.find_by(username:params[:username])
+        
+        if user&.authenticate(params[:password])
+
+            session[:current_user] = user.id
+
+            
+            render json: user, status: :ok
+        else
+
+
+
+            render json: { error: "Invalid Password and/or Username" },  status: :unauthorized
+        end
+    end 
+
+    def logout
+        session.delete :current_user
+    end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,10 @@
 Rails.application.routes.draw do
 
-
-  # resources :users, only: [:index, :show, :create, :destroy, :update]
   resources :phones, only: [:index, :show]
 
-  namespace :api do
-    namespace :v1 do
-      resources :users, only: [:create]
-      post '/login', to: 'auth#create'
-      get '/profile', to: 'users#profile'
-    end
-  end
-  # get "/teams", to: "teams#index"
-  # post "/login", to: "sessions#login"
-  # post "/signup", to: "users#create"
-  # get '/authorized_user', to: 'users#show'
-  # delete '/logout', to: 'sessions#logout'
+  post "/login", to: "sessions#login"
+  post "/signup", to: "users#create"
+  get '/authorized_user', to: 'users#show'
+  delete '/logout', to: 'sessions#logout'
+
 end
